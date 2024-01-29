2bf17de6d7a4b0830dbf5ff34bbe3f54d88a9a76
KhangTV02 <149467412+KhangTV02@users.noreply.github.com>
2024-01-30 00:38:14 +0700

    Portfolio final version

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -28,7 +28,7 @@ const Projects = () => {
     return (
     <div
       name="projects"
-      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen pt-80"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8 text-center">
